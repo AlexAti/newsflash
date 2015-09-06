1d7c9922d1a09de6ba6d49ed5baf6daae95de81e
Alex Ati <alejandro@atienzaramos.com>
2015-09-06 19:40:24 +0200

    ADDED: Source definition

--- src/newsflash/core.clj
+++ src/newsflash/core.clj
@@ -1,5 +1,11 @@
 (ns newsflash.core
   (:require [net.cgrand.enlive-html :as html]))
 
+(def ^:dynamic *sources* [["https://news.ycombinator.com" [:td.title :a] "EN"]])
+
+(defn grab-headlines [[url path language]]
+  "Grab headlines from a single source [url path language]"
+  (into [] (map (comp first :content) (html/select (html/html-resource (java.net.URL. url)) path))))
+
 (defn -main []
-  (println (html/select (html/html-resource (java.net.URL. "http://google.com")) [:title])))
+  (println (grab-headlines (first *sources*))))
