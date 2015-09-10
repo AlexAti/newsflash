0e4b8a7613ca2a85c52ed5c11f16771a4904337e
Alex Ati <alejandro@atienzaramos.com>
2015-09-10 19:44:31 +0200

    ADDED: Read the headlines

--- src/newsflash/core.clj
+++ src/newsflash/core.clj
@@ -1,11 +1,24 @@
 (ns newsflash.core
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [clojure.java.shell :refer [sh]]
+            [net.cgrand.enlive-html :as html]))
 
 (def ^:dynamic *sources* [["https://news.ycombinator.com" [:td.title :a] "EN"]])
+
+(def ^:dynamic *readers* [["Mac OS X" "ES" ["say" "-v" "Monica"]]
+                          ["Mac OS X" "EN" ["say" "-v" "Samantha"]]])
 
 (defn grab-headlines [[url path language]]
   "Grab headlines from a single source [url path language]"
   (into [] (map (comp first :content) (html/select (html/html-resource (java.net.URL. url)) path))))
 
+(defn read-aloud-headline [headline os language]
+  (let [goodreader? (fn [[o l c]] (and (= o os) (= l language)))
+        [o l command] (first (filter goodreader? *readers*))]
+    (println (str "Reading " headline "..."))
+    (apply sh (concat command [:in headline]))
+    (sh "sleep" "1")))
+
 (defn -main []
-  (println (grab-headlines (first *sources*))))
+  (let [os (System/getProperty "os.name")]
+    (doseq [hd (grab-headlines (first *sources*))]
+      (read-aloud-headline hd os "EN"))))
